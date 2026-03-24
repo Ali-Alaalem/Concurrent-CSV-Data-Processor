188f114351e3921aff474b6b54c9d8a5fd2988ac
Ali-Alaalem <meemo2002a@gmail.com>
2026-03-24 13:51:05 +0300

    first commit

---
+++ src/test/java/com/Concurrency_Mini_Project/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Concurrency_Mini_Project.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
