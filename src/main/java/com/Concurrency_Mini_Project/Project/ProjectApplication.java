e881474e82f8dda456752a22368f3be1f75172c7
Ali-Alaalem <meemo2002a@gmail.com>
2026-03-27 13:55:16 +0300

    adding the csv reader and the csv file calling them in main to test the reader

--- src/main/java/com/Concurrency_Mini_Project/Project/ProjectApplication.java
+++ src/main/java/com/Concurrency_Mini_Project/Project/ProjectApplication.java
@@ -1,13 +1,22 @@
 package com.Concurrency_Mini_Project.Project;
 
+import com.Concurrency_Mini_Project.Project.Models.Employee;
+import com.Concurrency_Mini_Project.Project.Services.CsvReaderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+        SpringApplication.run(ProjectApplication.class, args);
+
+        CsvReaderService reader = new CsvReaderService();
+        List<Employee> employees = reader.readCSV();
+        employees.forEach(System.out::println);
+
 	}
 
 }
---
+++ src/main/java/com/Concurrency_Mini_Project/Project/Services/CsvReaderService.java
@@ -0,0 +1,46 @@
+package com.Concurrency_Mini_Project.Project.Services;
+
+
+import com.Concurrency_Mini_Project.Project.Exceptions.InvalidCSVException;
+import com.Concurrency_Mini_Project.Project.Models.Employee;
+import org.springframework.stereotype.Service;
+
+
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Service
+public class CsvReaderService {
+
+    public List<Employee> readCSV() {
+        List<Employee> employees = new ArrayList<>();
+        File file = new File("Employees.csv");
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] user = line.split(",");
+
+                Employee employee = new Employee();
+
+                employee.setName(user[0]);
+                employee.setSalary(Double.parseDouble(user[1]));
+                employee.setJoinedDate(LocalDate.parse(user[2]));
+                employee.setRole(user[3]);
+                employee.setProjectCompletionPercentage(Double.parseDouble(user[4]));
+
+                employees.add(employee);
+            }
+
+        } catch (Exception e) {
+            throw new InvalidCSVException("The Csv file is not exist or have bad data");
+        }
+
+        return employees;
+    }
+}
